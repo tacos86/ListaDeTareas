91696114e8ef8c0dac9ef9240ac2ec2e29b9ce01
bcosta <49333564j@gmail.com>
2021-08-01 18:46:46 -0300

    borra tarea

--- archivo.js
+++ archivo.js
@@ -22,3 +22,8 @@ tareaAgregada.addEventListener('submit', e => {
     ponerTareaEnLaLista(nuevaTarea);
 });
 
+lista.addEventListener('click', e => {
+    if(e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+});
