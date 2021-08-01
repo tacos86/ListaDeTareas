7cd233ba154e3bbf7ed1a8e8e233c33c4d88b130
bcosta <49333564j@gmail.com>
2021-08-01 17:06:54 -0300

    agrega la tarea

--- archivo.js
+++ archivo.js
@@ -5,3 +5,25 @@ tareaAgregada.addEventListener('submit', e => {
     const nuevaTarea = tareaAgregada.agregarTarea.value;
     console.log(nuevaTarea);
 });
+
+const lista = document.querySelector('.mostrarTarea');
+const ponerTareaEnLaLista = tarea => {
+    const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${tarea}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+    lista.innerHTML += html;
+};
+tareaAgregada.addEventListener('submit', e => {
+    e.preventDefault();
+    const nuevaTarea = tareaAgregada.agregarTarea.value.trim();
+    ponerTareaEnLaLista(nuevaTarea);
+});
+
+lista.addEventListener('click', e => {
+    if(e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+});
