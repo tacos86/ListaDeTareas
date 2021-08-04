a88daa1606355b5e1ffdd4d5ae092d3147062985
bcosta <49333564j@gmail.com>
2021-08-03 22:24:49 -0300

    codigo ordenado

--- archivo.js
+++ archivo.js
@@ -1,29 +1,41 @@
-const tareaAgregada = document.querySelector('.agregarTarea');
- 
-tareaAgregada.addEventListener('submit', e => {
-    e.preventDefault();
-    const nuevaTarea = tareaAgregada.agregarTarea.value;
-    console.log(nuevaTarea);
-});
+//VARIABLES
+//Selectores
 
+//selecciona el input
+const tareaAgregada = document.querySelector('.agregarTarea');
+//selecciona la lista donde se va a agregar la tarea
 const lista = document.querySelector('.mostrarTarea');
-const ponerTareaEnLaLista = tarea => {
-    const html = `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>${tarea}</span>
-        <i class="far fa-trash-alt delete"></i>
-    </li>
-    `;
-    lista.innerHTML += html;
-};
+
+//EVENTOS
+//obtiene el valor del input al enviar el formulario 
 tareaAgregada.addEventListener('submit', e => {
     e.preventDefault();
     const nuevaTarea = tareaAgregada.agregarTarea.value.trim();
     ponerTareaEnLaLista(nuevaTarea);
+    //resetea el input
+    tareaAgregada.reset();        
 });
-
+//borra la tarea al darle click al tachito
 lista.addEventListener('click', e => {
     if(e.target.classList.contains('delete')) {
         e.target.parentElement.remove();
     }
 });
+
+//FUNCION
+//agrega la tarea a la lista
+const ponerTareaEnLaLista = tarea => {
+    const texto = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${tarea}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+    lista.innerHTML += texto;
+};
+
+
+
+
+
+
